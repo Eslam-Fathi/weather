40233b12103b16ffbec8bb156a7705d945a5fa1f
Eslam Fathi <efathi2511@gmail.com>
2024-03-28 17:20:59 +0200

    Add new screens and dependencies for weather app
    This commit adds a new home screen for the weather app along with necessary
    dependencies like bloc, google_fonts, flutter_svg, and more. It also includes
    assets for the new screens.

---
+++ lib/View/Screens/home_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true; // Keep the state alive
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    return Scaffold(
+      floatingActionButton: Padding(
+        padding: EdgeInsets.symmetric(vertical: 30.h),
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          shape: const CircleBorder(),
+          child: SvgPicture.asset("assets/svg/svgicon/Plus.svg"),
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      body: Stack(
+        children: [
+          // Background image
+          Image.asset(
+            'assets/png/background/Image2.png', //  Background image
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+
+          Align(
+            alignment: const Alignment(0, .3),
+            child: Image.asset(
+              'assets/png/background/House.png', //  House image
+              width: 390.w,
+              height: 390.h,
+              // fit: BoxFit.fill,
+            ),
+          ),
+
+          // Text Content at the top of the house
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50.h,
+                ),
+                Text(
+                  'Ismailia',
+                  style: TextStyle(
+                    fontSize: 34.sp,
+                    fontFamily: GoogleFonts.nunitoSans().fontFamily,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '20°',
+                  style: TextStyle(
+                    fontFamily: GoogleFonts.nunitoSans().fontFamily,
+                    fontSize: 96.sp,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "Clear",
+                  style: TextStyle(
+                    fontFamily: GoogleFonts.nunitoSans().fontFamily,
+                    fontSize: 20.sp,
+                    color: Colors.white70,
+                  ),
+                ),
+                Text(
+                  'H: 18°  L: 22°',
+                  style: TextStyle(
+                    fontFamily: GoogleFonts.nunitoSans().fontFamily,
+                    fontSize: 20.sp,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            // floating button background
+            left: 0,
+            right: 0,
+            bottom: -10,
+            child: SvgPicture.asset("assets/svg/Back.svg", width: 400.w),
+          ),
+
+          Positioned(
+            // buttons background
+            left: 0,
+            right: 0,
+            bottom: -10,
+            child: SvgPicture.asset("assets/svg/Subtract.svg"),
+          ),
+
+          //TODO: implement buttons' functions here
+
+          const Positioned(
+              left: 0, right: 300, bottom: 20, child: Placeholder()),
+          const Positioned(
+              left: 300, right: 0, bottom: 20, child: Placeholder()),
+        ],
+      ),
+    );
+  }
+}
